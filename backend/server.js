bfaaefb90bea3dc0cbc8a865354c118e902738fb
Ioana Mitelea <miteleaioana@gmail.com>
2019-03-21 20:46:13 +0000

    Bind server to

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 const mongoose = require("./db");
 const apiRouter = require("./routes/api");
