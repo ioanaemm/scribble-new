a03f2fae364454ec5bd6d1b99ec6cc63e3b6c7c5
Ioana Mitelea <miteleaioana@gmail.com>
2019-03-31 21:47:57 +0100

    Add unit test for the user component

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -12,17 +12,6 @@ export default class User extends Component {
     this.onKeyDown = this.onKeyDown.bind(this);
   }
 
-  // componentDidMount() {
-  //   Api.fetchUserDetails().then(
-  //     response => {
-  //       this.setState({ userData: response.data, pending: false });
-  //     },
-  //     error => {
-  //       this.setState({ pending: false });
-  //     }
-  //   );
-  // }
-
   onKeyDown(e) {
     console.log("onFormSubmit()");
     if (e.key !== "Enter") {
@@ -43,27 +32,22 @@ export default class User extends Component {
   }
 
   render() {
-    if (this.state.pending) {
-      return <p>Loading...</p>;
-    }
-    if (!this.state.userData) {
-      return (
-        <form onKeyDown={this.onKeyDown} className="login">
-          <input
-            className="username"
-            value={this.state.username}
-            placeholder="Username"
-            onChange={e => this.setState({ username: e.target.value })}
-          />
-          <input
-            className="password"
-            value={this.state.password}
-            type="password"
-            placeholder="Password"
-            onChange={e => this.setState({ password: e.target.value })}
-          />
-        </form>
-      );
-    }
+    return (
+      <form onKeyDown={this.onKeyDown} className="login">
+        <input
+          className="username"
+          value={this.state.username}
+          placeholder="Username"
+          onChange={e => this.setState({ username: e.target.value })}
+        />
+        <input
+          className="password"
+          value={this.state.password}
+          type="password"
+          placeholder="Password"
+          onChange={e => this.setState({ password: e.target.value })}
+        />
+      </form>
+    );
   }
 }
---
+++ src/components/User/User.test.js
@@ -0,0 +1,65 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import User from "components/User/User";
+import { shallow } from "enzyme";
+
+import * as Api from "Api/Api";
+
+describe("User component", () => {
+  it("renders without crashing", () => {
+    const wrapper = shallow(<User />);
+  });
+
+  it("renders the username input", () => {
+    const wrapper = shallow(<User />);
+    expect(wrapper.find(".username").exists()).toBe(true);
+  });
+
+  it("renders the password input", () => {
+    const wrapper = shallow(<User />);
+    expect(wrapper.find(".password").exists()).toBe(true);
+  });
+
+  it("the username input works", () => {
+    const wrapper = shallow(<User />);
+    wrapper
+      .find(".username")
+      .simulate("change", { target: { value: "ioanam" } });
+    expect(wrapper.find(".username").prop("value")).toEqual("ioanam");
+  });
+
+  it("the password input works", () => {
+    const wrapper = shallow(<User />);
+    wrapper
+      .find(".password")
+      .simulate("change", { target: { value: "qwerty" } });
+    expect(wrapper.find(".password").prop("value")).toEqual("qwerty");
+  });
+
+  it("does not submit the form", () => {
+    const mockSignInUser = jest.fn();
+    // when a non-enter key is pressed,
+    // the Api.signInUser function doesn't get called
+    Api.signInUser = mockSignInUser;
+    const wrapper = shallow(<User />);
+    wrapper.find(".login").simulate("keydown", { key: "Shift" });
+    expect(mockSignInUser).not.toBeCalled();
+  });
+
+  it("submits the form", async () => {
+    Api.signInUser = jest.fn().mockReturnValue(
+      new Promise((resolve, reject) => {
+        resolve({
+          data: {
+            foo: "test"
+          }
+        });
+      })
+    );
+    const mockOnLogin = jest.fn();
+    const wrapper = shallow(<User onLogin={mockOnLogin} />);
+    wrapper.find(".login").simulate("keydown", { key: "Enter" });
+    await new Promise(resolve => resolve());
+    expect(mockOnLogin).toBeCalledWith({ foo: "test" });
+  });
+});
