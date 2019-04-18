827b8a6fdb87cb6ab9056a3569e4aaaf17dd3caa
Ioana Mitelea <miteleaioana@gmail.com>
2019-04-18 08:28:20 +0100

    Add sentry integration

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import "./index.css";
 import App from "components/App/App";
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://e0819b1bf06247caba3ce6d125bd18f4@sentry.io/1441787"
+});
 
 ReactDOM.render(
   <BrowserRouter>
